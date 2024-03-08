89cc3b8661064af4b965aea4a3a5d7c7df8e1b38
sdd1whu <diandian.sun@bitech-automotive.com>
2024-03-08 17:07:13 +0800

    ✨ feat: update demo

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,42 @@ import dataSource from "../public/project_data_month_project_202310.json";
 
 function App() {
   return (
-    <EasyCharts<any>
-      onDataSourceChange={async () => {
-        return {
-          dataDict: [],
-          dataSource: dataSource,
-        };
+    <div
+      style={{
+        height: "500px",
+        margin: "50px 64px",
+        width: "50%",
+        border: "1px solid #eee",
+        padding: "8px",
+        borderRadius: "8px",
       }}
-    />
+    >
+      <EasyCharts<any>
+        onDataSourceChange={async () => {
+          return {
+            dataDict: [
+              {
+                field: "bug_all",
+                displayName: "Bug总数",
+              },
+              {
+                field: "bug_resolved",
+                displayName: "解决Bug总数",
+              },
+              {
+                field: "bug_resolved_rate",
+                displayName: "Bug修复率",
+              },
+              {
+                field: "project_name",
+                displayName: "项目名",
+              },
+            ],
+            dataSource: dataSource,
+          };
+        }}
+      />
+    </div>
   );
 }
 
