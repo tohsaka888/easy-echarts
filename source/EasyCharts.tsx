6f661c04e855c09374570e8b75224411d2045bae
sdd1whu <diandian.sun@bitech-automotive.com>
2024-03-08 17:05:11 +0800

    ✨ feat: add config

--- source/EasyCharts.tsx
+++ source/EasyCharts.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useMemo } from "react";
-import ChartConfigProvider from "./context/ChartConfigProvider";
+import {
+  ChartConfigProvider,
+  PreviewChartConfigProvider,
+} from "./context/ChartConfigProvider";
 import ConfigArea from "./ConfigArea";
 import Chart from "./Chart";
 import { EasyChartsProps } from ".";
@@ -13,19 +16,26 @@ function EasyCharts<T extends object>(props: EasyChartsProps<T>) {
   return (
     <ChartDataProvider>
       <ChartConfigProvider>
-        <PropsProvider initialValues={props}>
-          <ModalLoadingProvider>
-            <SegmentedProvider>
-              <ConfigArea>
-                <Chart
-                  initDataSource={props.initDataSource}
-                  key={"config-chart"}
-                />
-              </ConfigArea>
-            </SegmentedProvider>
-          </ModalLoadingProvider>
-          <Chart initDataSource={props.initDataSource} key={"preview-chart"} />
-        </PropsProvider>
+        <PreviewChartConfigProvider>
+          <PropsProvider initialValues={props}>
+            <ModalLoadingProvider>
+              <SegmentedProvider>
+                <ConfigArea>
+                  <Chart
+                    mode="config"
+                    initDataSource={props.initDataSource}
+                    key={"config-chart"}
+                  />
+                </ConfigArea>
+              </SegmentedProvider>
+            </ModalLoadingProvider>
+            <Chart
+              mode="preview"
+              initDataSource={props.initDataSource}
+              key={"preview-chart"}
+            />
+          </PropsProvider>
+        </PreviewChartConfigProvider>
       </ChartConfigProvider>
     </ChartDataProvider>
   );
